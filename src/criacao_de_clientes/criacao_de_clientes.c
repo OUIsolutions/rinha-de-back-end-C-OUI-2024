8186367cfff8e8f03fe2b4243e19f867d1402640
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-03-07 17:23:26 -0300

    at

--- src/criacao_de_clientes/criacao_de_clientes.c
+++ src/criacao_de_clientes/criacao_de_clientes.c
@@ -1,30 +1,35 @@
 
 CxpathJson * obter_clientes_default(){
     CxpathJson * started = xpath.newJsonArray();
-    xpath.set_int(started,0,"['$append',%d]",SALDO_INDEX);
-    xpath.set_int(started,100000,"['-1',%d]",LIMITE_INDEX);
-    xpath.set_int(started,0,"['-1',%d]",TOTAL_TRANSACOES_INDEX);
-    xpath.set_int(started,0,"['-1',%d]",ULTIMA_TRANSACAO_INDEX);
+    xpath.set_int(started,0,"['$append','$append']"); //saldo
+    xpath.set_int(started,100000,"[-1,'$append']"); //limite
+    xpath.set_int(started,0,"[-1,'$append']"); //total transacoes
+    xpath.set_int(started,0,"[-1,'$append']"); // ultima transacao
 
-    xpath.set_int(started,0,"['$append',%d]",SALDO_INDEX);
-    xpath.set_int(started,80000,"['-1',%d]",LIMITE_INDEX);
-    xpath.set_int(started,0,"['-1',%d]",TOTAL_TRANSACOES_INDEX);
-    xpath.set_int(started,0,"['-1',%d]",ULTIMA_TRANSACAO_INDEX);
 
-    xpath.set_int(started,0,"['$append',%d]",SALDO_INDEX);
-    xpath.set_int(started,1000000,"['-1',%d]",LIMITE_INDEX);
-    xpath.set_int(started,0,"['-1',%d]",TOTAL_TRANSACOES_INDEX);
-    xpath.set_int(started,0,"['-1',%d]",ULTIMA_TRANSACAO_INDEX);
 
-    xpath.set_int(started,0,"['$append',%d]",SALDO_INDEX);
-    xpath.set_int(started,10000000,"['-1',%d]",LIMITE_INDEX);
-    xpath.set_int(started,0,"['-1',%d]",TOTAL_TRANSACOES_INDEX);
-    xpath.set_int(started,0,"['-1',%d]",ULTIMA_TRANSACAO_INDEX);
+    xpath.set_int(started,0,"['$append','$append']"); //saldo
+    xpath.set_int(started,80000,"[-1,'$append']"); //limite
+    xpath.set_int(started,0,"[-1,'$append']"); //total transacoes
+    xpath.set_int(started,0,"[-1,'$append']"); // ultima transacao
 
-    xpath.set_int(started,0,"['$append',%d]",SALDO_INDEX);
-    xpath.set_int(started,500000,"['-1',%d]",LIMITE_INDEX);
-    xpath.set_int(started,0,"['-1',%d]",TOTAL_TRANSACOES_INDEX);
-    xpath.set_int(started,0,"['-1',%d]",ULTIMA_TRANSACAO_INDEX);
+
+    xpath.set_int(started,0,"['$append','$append']"); //saldo
+    xpath.set_int(started,1000000,"[-1,'$append']"); //limite
+    xpath.set_int(started,0,"[-1,'$append']"); //total transacoes
+    xpath.set_int(started,0,"[-1,'$append']"); // ultima transacao
+
+    xpath.set_int(started,0,"['$append','$append']"); //saldo
+    xpath.set_int(started,10000000,"[-1,'$append']"); //limite
+    xpath.set_int(started,0,"[-1,'$append']"); //total transacoes
+    xpath.set_int(started,0,"[-1,'$append']"); // ultima transacao
+
+    xpath.set_int(started,0,"['$append','$append']"); //saldo
+    xpath.set_int(started,500000,"[-1,'$append']"); //limite
+    xpath.set_int(started,0,"[-1,'$append']"); //total transacoes
+    xpath.set_int(started,0,"[-1,'$append']"); // ultima transacao
+
+
 
     return started;
 }
@@ -39,11 +44,15 @@ void cria_clientes_inicias(){
     CxpathJson *clientes =obter_clientes_default();
     UniversalGarbage_add(garbage, xpath.free,clientes);
 
+
     char *json_parsed = NULL;
     UniversalGarbage_add_simple(garbage,json_parsed);
     int total = xpath.size(clientes,"[]");
+
     for(int i = 0; i < total; i++){
         CxpathJson  * atual = xpath.get_array(clientes,"[%d]",i);
+
+
         DtwResource * resource_cliente = DtwResource_sub_resource(banco,"%d",i+1);
         json_parsed =  xpath.dump_to_string(atual,false);
         UniversalGarbage_resset(garbage,json_parsed);
