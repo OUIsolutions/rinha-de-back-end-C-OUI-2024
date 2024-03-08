0fc2cd7e711736d26816c02750ee36f3afe50da6
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-03-08 14:18:29 -0300

    a

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
+./a.out 3001 & ./a.out & 3002
 
-
-for port in $(seq 3001 $((3001 + $TOTAL_APIS - 1))); do
-  ./a.out $port &
-done
+#for port in $(seq 3001 $((3001 + $TOTAL_APIS - 1))); do
+#  ./a.out $port &
+#done
