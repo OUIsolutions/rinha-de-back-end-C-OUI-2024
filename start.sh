362214f66ddd74175f44817414d20d2dff163f33
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-03-08 15:02:22 -0300

    at

--- start.sh
+++ start.sh
@@ -6,8 +6,9 @@ result=""
 total=3000
 for counter in $(seq 1 ${total_menos_um});  do 
     total=$((total +1))
-    result="${result} ./a.out ${total} &"
+    result="${result} ./a.out ${total} & "
+
  done
 total=$((total +1))
 result="${result} ./a.out ${total}";
-echo $result
+eval $result
